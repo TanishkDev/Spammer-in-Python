89ebdd883ee28cdbfc7c3959a71497e5354b6acc
TanishkDev <78966701+TanishkDev@users.noreply.github.com>
2021-09-10 21:28:23 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,5 +1,6 @@
 import pyautogui,time
+text = input()
 time.sleep(5)
 for i in range(5):
-    pyautogui.typewrite("codding sikh li")
+    pyautogui.typewrite(text)#your text that would be a spam message
     pyautogui.press('enter')
